bcd40ce51239bd3eceb7b2462069323b357cd7ac
Gustavo Galvão <gustavojezler@gmail.com>
2025-11-10 23:01:06 -0300

    Update img/layout

---
+++ assets/scripts/script.js
@@ -0,0 +1,29 @@
+/*=========== numeros subindo ==========*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".stat-card__num");
+
+  counters.forEach(counter => {
+    const target = +counter.textContent;
+    counter.textContent = "0";
+
+    const duration = 4000;
+    const steps = 240;
+    const increment = target / steps;
+    let current = 0;
+    let count = 0;
+
+    const updateCounter = () => {
+      current += increment;
+      count++;
+      if (count < steps) {
+        counter.textContent = Math.floor(current);
+        requestAnimationFrame(updateCounter);
+      } else {
+        counter.textContent = target;
+      }
+    };
+
+    requestAnimationFrame(updateCounter);
+  });
+});
